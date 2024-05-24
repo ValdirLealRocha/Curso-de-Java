a5c0c5856f31fcf79551452b5fca6463e4971581
ValdirLealRocha <valdirlealrocha@hotmail.com>
2024-05-24 16:58:42 -0300

    Atualização Aula 13
    Atualização Aula 13

--- CursoJavaAula13.java
+++ CursoJavaAula13.java
@@ -1,39 +1,39 @@
 // Aula 13 - https://youtu.be/TW5FthKNr50?list=PLx4x_zx8csUjFC5WWjoNUL7LOOD7LCKRW
 // Métodos e atributos STATIC - Curso de Java - Aula 13
+// desta aula em diante irei eliminar as linhas comentadas da aula anterior, 
+// caso não faça sentido para esta aula, assim manterei mais limpo o código 
+// e meu aprendizado mais claro.
+//
+// NOTA: Para ter mais clareza no código cada classe jogador será redefinida para 
+// cada aula em diante, nesta aula vale a classe JogadorAula13.java
+//
 public class CursoJavaAula13 {
 
   public static void main(String[] args) {
     
-    int num = 0; // num está em escopo diferente das classes e não causa erro de duplicidade
+    int num = 0;
 
-    // Instanciar um objeto, é como iniciar uma variável.
-    // j1 está recebendo um novo objeto da classe Jogador,
-    // a palavra new é quem cria a instancia da classe e aloca 
-    // memória 'heap' para execução do objeto.
-    // tipo - Jogador
-    // nome - j1
-    // recebe valor - = new Jogador();
-    // 3 Objetos j1, j2 e j3 instanciados separadamente.
-    // Jogador j1 = new Jogador(1);
-    // Jogador j2 = new Jogador(2);  
-    // Jogador j1 = new Jogador(num++); // pós incremento
-    // Jogador j2 = new Jogador(num++);  
-    // Jogador j3 = new Jogador(num++);  
-    Jogador j1 = new Jogador(++num); // pré incremento
-    Jogador j2 = new Jogador(++num);  
-    Jogador j3 = new Jogador(++num);  
+    // Está sendo usado a propriedade alerta, antes de instanciar a classe de JogadorAula13
+    //System.out.printf("%nAlerta: %s%n", (JogadorAula13.alerta ? "Sim" : "Não"));
 
-    // vidas sendo inicializada fora da classe, através do método setVidas
-    // validação das vidas, não deve ser maior que 3...
-    j1.addVidas(); // outra fora de inicializar a propriedade da classe através de incremento em addVidas
-    j1.addVidas(); // outra fora de inicializar a propriedade da classe através de incremento em addVidas
-    j1.addVidas(); // outra fora de inicializar a propriedade da classe através de incremento em addVidas
-    j1.addVidas(); // outra fora de inicializar a propriedade da classe através de incremento em addVidas
-    j2.addVidas(); // outra fora de inicializar a propriedade da classe através de incremento em addVidas
-    j3.addVidas(); // outra fora de inicializar a propriedade da classe através de incremento em addVidas
+    JogadorAula13.pontos();
+    JogadorAula13.pontos();
+    JogadorAula13.pontos();
 
-    System.out.printf("%nVidas do jogador 1: %d",j1.getVidas());
-    System.out.printf("%nVidas do jogador 2: %d",j2.getVidas());
-    System.out.printf("%nVidas do jogador 3: %d",j3.getVidas());
+    JogadorAula13 j1 = new JogadorAula13(++num);
+    JogadorAula13 j2 = new JogadorAula13(++num);
+    JogadorAula13 j3 = new JogadorAula13(++num);
+
+    // inicializado fora da classe...
+    JogadorAula13.alerta = true;
+
+    // todos estes objetos estão utilizando o mesmo endereço de memória do alerta, 
+    // porque ele é static na classe.
+    j1.info();
+    j2.info();
+    j3.info();
+
+    //System.out.printf("%nVidas do jogador 1: %d",j1.getVidas());
+
   }
 }
